5654486be5b7f0321e6fdd54591a94b00e18b151
LimitedSky <cruel2flow@naver.com>
2020-06-24 20:10:59 +0900

    first frame

--- src/teamP/Main.java
+++ src/teamP/Main.java
@@ -1,10 +1,9 @@
-package teamP.main;
+package teamP;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		InputFrame inputFrame = new InputFrame();
 	}
 
 }
