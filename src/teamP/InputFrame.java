b9dad912c26712f5b8fd4426f6522b52496ba021
LimitedSky <cruel2flow@naver.com>
2020-06-24 20:16:16 +0900

    first frame

---
+++ src/teamP/InputFrame.java
@@ -0,0 +1,66 @@
+package teamP;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class InputFrame extends JFrame {
+
+	private final int width = 300;
+	private final int height = 150;
+
+	private JTextField widthCount;
+	private JTextField heightCount;
+	private JLabel multiply;
+	private JButton startBtn;
+	private Labrinth lab;
+
+	public InputFrame() {
+		// get screen size
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int xPos = (int) ((screenSize.getWidth() - width) / 2);
+		int yPos = (int) ((screenSize.getHeight() - height) / 2);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("창 크기 설정");
+		setBounds(xPos, yPos, width, height);
+		setSize(width, height);
+
+		multiply = new JLabel("x");
+		widthCount = new JTextField(3);
+		heightCount = new JTextField(3);
+		startBtn = new JButton("start!!");
+
+		startBtn.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				lab = new Labrinth(Integer.parseInt(widthCount.getText()), Integer.parseInt(heightCount.getText()));
+				if (lab != null)
+					System.out.println("make labrinth");
+			}
+		});
+
+		widthCount.setFont(new Font("고딕", Font.PLAIN, 25));
+		heightCount.setFont(new Font("고딕", Font.PLAIN, 25));
+		multiply.setFont(new Font("고딕", Font.PLAIN, 25));
+		startBtn.setFont(new Font("고딕", Font.PLAIN, 25));
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+
+		c.add(widthCount);
+		c.add(multiply);
+		c.add(heightCount);
+		c.add(startBtn);
+
+		setVisible(true);
+	}
+
+}
---
+++ src/teamP/Labrinth.java
@@ -0,0 +1,13 @@
+package teamP;
+
+public class Labrinth {
+	
+	private int widthCount;
+	private int heightCount;
+	
+	public Labrinth(int widthCount, int heightCount) {
+		this.widthCount = widthCount;
+		this.heightCount = heightCount;
+	}
+	
+}
